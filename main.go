81413ca67ff244cdc67888a82c12eaa9297cd107
Krishna Chaithanya <chaitanyam.be20@uceou.edu>
2023-10-09 23:44:48 -0400

    feat(open-file):checking if git-profile is already created

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
-import "fmt"
 import "os"
+import "log"
+
+func checkGitProfile(){
+  fd,err := os.Open("~/.git-profile");
+  defer fd.Close();
+  if err!= nil{
+    log.Println("creating the git profile spaces");
+    err := os.Mkdir(os.ExpandEnv("$HOME/.git-profile"),0755);
+    if err !=nil {
+      log.Fatalln(err);
+    }
+  }
+}
 
 func main()  {
   // check if the command is run as a sudo user
   uid := os.Getuid()
   if uid!=0 {
-    fmt.Println("Please run this as a sudo user");
-    os.Exit(1)
+    log.Fatalln("Please run this as a sudo user");
   }
-  fmt.Println("Running as sudo user, moving on");
+  log.Println("sudo user please move on");
+  // check if the .git-profile is present, if not create it 
+  checkGitProfile();
 }
